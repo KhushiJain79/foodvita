e273bf3cb06ad61e73b8a7a0b317a4cc4835c863
Khushi Jain <khushijain3579@gmail.com>
2024-04-03 17:32:23 +0530

    demo changes

--- foodapp/backend/db.js
+++ foodapp/backend/db.js
@@ -1,7 +1,7 @@
 
 const mongoose = require("mongoose");
 const db =
-  "mongodb://localhost:27017/fooddatabase";
+  "mongodb+srv://food:AB9$NhmjK4bfJcB@cluster0.gnmz8hz.mongodb.net/fooddatabase";
 
 mongoose.set("strictQuery", true, "useNewUrlParser", true);
 
